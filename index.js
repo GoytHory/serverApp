ac8fd7514f1be336827f676bb8bbd74c34df5e38
GregoryZexon <gregoryzexon@gmail.com>
2026-03-21 10:33:03 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*", // Разрешаем всем подключаться
+    methods: ["GET", "POST"]
+  }
+});
+
+io.on('connection', (socket) => {
+  console.log('КТО-ТО ПОДКЛЮЧИЛСЯ! ID:', socket.id);
+
+  socket.on('message', (data) => {
+    console.log('--- НОВОЕ СООБЩЕНИЕ ---');
+    console.log('От кого:', data.senderName);
+    console.log('Текст:', data.text);
+    
+    // Чтобы сообщение увидели ВСЕ, сервер должен его переслать обратно
+    io.emit('message', data); 
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Юзер ушел');
+  });
+});
+
+// ВХОД: Берем порт, который даст Render, или 3000, если запускаем дома
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, '0.0.0.0', () => {
+  console.log(`Сервер запущен на порту ${PORT}`);
+});
